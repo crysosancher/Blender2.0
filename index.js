a2e8c8dfef0c65f6f8dd0bb97cc7fc6234283431
Mahesh Kumar <81576386+jacktheboss220@users.noreply.github.com>
2022-02-13 10:25:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1027,52 +1027,59 @@ async function main() {
                         if (!isGroup) return;
                         var ref = args[0];
                         console.log(`${ref}`)
-                        // Global constants
                         const tracker = {
                             start: Date.now(),
                             audio: { downloaded: 0, total: Infinity },
                             video: { downloaded: 0, total: Infinity },
+                            merged: { frame: 0, speed: '0x', fps: 0 },
                         };
 
-                        // Get audio and video stream going
-                        const audio = ytdl(ref, { filter: 'audioonly', quality: 'highestaudio' })
+                        // Get audio and video streams
+                        const audio = ytdl(ref, { quality: 'highestaudio' })
                             .on('progress', (_, downloaded, total) => {
                                 tracker.audio = { downloaded, total };
                             });
-                        const video = ytdl(ref, { filter: 'videoonly', quality: 'highestvideo' })
+                        const video = ytdl(ref, { quality: 'highestvideo' })
                             .on('progress', (_, downloaded, total) => {
                                 tracker.video = { downloaded, total };
                             });
 
-                        // Get the progress bar going
-                        const progressbar = setInterval(() => {
+                        // Prepare the progress bar
+                        let progressbarHandle = null;
+                        const progressbarInterval = 1000;
+                        const showProgress = () => {
                             readline.cursorTo(process.stdout, 0);
                             const toMB = i => (i / 1024 / 1024).toFixed(2);
 
-                            process.stdout.write(`Audio | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+                            process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
                             process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
 
-                            process.stdout.write(`Video | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+                            process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
                             process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
 
+                            process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+                            process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
+
                             process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
-                            readline.moveCursor(process.stdout, 0, -2);
-                        }, 1000);
+                            readline.moveCursor(process.stdout, 0, -3);
+                        };
 
                         // Start the ffmpeg child process
                         const ffmpegProcess = cp.spawn(ffmpeg, [
                             // Remove ffmpeg's console spamming
-                            '-loglevel', '0', '-hide_banner',
-                            // 3 second audio offset
-                            '-itsoffset', '3.0', '-i', 'pipe:3',
+                            '-loglevel', '8', '-hide_banner',
+                            // Redirect/Enable progress messages
+                            '-progress', 'pipe:3',
+                            // Set inputs
                             '-i', 'pipe:4',
-                            // Rescale the video
-                            '-vf', 'scale=320:240',
-                            // Choose some fancy codes
-                            '-c:v', 'libx265', '-x265-params', 'log-level=0',
-                            '-c:a', 'flac',
-                            // Define output container
-                            '-f', 'matroska', 'pipe:5',
+                            '-i', 'pipe:5',
+                            // Map audio & video from streams
+                            '-map', '0:a',
+                            '-map', '1:v',
+                            // Keep encoding
+                            '-c:v', 'copy',
+                            // Define output file
+                            'out.mkv',
                         ], {
                             windowsHide: true,
                             stdio: [
@@ -1083,20 +1090,28 @@ async function main() {
                             ],
                         });
                         ffmpegProcess.on('close', () => {
-                            process.stdout.write('\n\n\n');
-                            clearInterval(progressbar);
                             console.log('done');
+                            // Cleanup
+                            process.stdout.write('\n\n\n\n');
+                            clearInterval(progressbarHandle);
                         });
 
                         // Link streams
                         // FFmpeg creates the transformer streams and we just have to insert / read data
-                        audio.pipe(ffmpegProcess.stdio[3]);
-                        video.pipe(ffmpegProcess.stdio[4]);
-                        ffmpegProcess.stdio[5].pipe(fs.createWriteStream('out.mkv'));
-                        await new Promise((resolve, reject) => {
-                            stream.on('error', reject)
-                            stream.on('finish', resolve)
-                        }).then(async (res) => {
+                        ffmpegProcess.stdio[3].on('data', chunk => {
+                            // Start the progress bar
+                            if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
+                            // Parse the param=value list returned by ffmpeg
+                            const lines = chunk.toString().trim().split('\n');
+                            const arg = {};
+                            for (const l of lines) {
+                                const [key, value] = l.split('=');
+                                arg[key.trim()] = value.trim();
+                            }
+                            tracker.merged = arg;
+                        });
+                        audio.pipe(ffmpegProcess.stdio[4]);
+                        video.pipe(ffmpegProcess.stdio[5]);
                             await conn.sendMessage(
                                 from,
                                 fs.readFileSync('out.mkv'),
