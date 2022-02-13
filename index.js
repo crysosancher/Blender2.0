7ad0951fd0a96cbcb8a3984f5e2ec96365dd5921
Mahesh Kumar <81576386+jacktheboss220@users.noreply.github.com>
2022-02-13 10:35:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const ud = require('urban-dictionary')
 const inshorts = require('inshorts-api');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-const cp = require('child_process');
-const readline = require('readline');
 const yahooStockPrices = require('yahoo-stock-prices');
 const port = process.env.PORT || 8000;
 server.get('/', (req, res) => { res.send('V-Bot server running...') })
@@ -1021,106 +1019,6 @@ async function main() {
                         let genderText = await getGender(namePerson);
                         reply(genderText);
                         break;
-
-
-                    case 'yth':
-                        if (!isGroup) return;
-                        var ref = args[0];
-                        console.log(`${ref}`)
-                        const tracker = {
-                            start: Date.now(),
-                            audio: { downloaded: 0, total: Infinity },
-                            video: { downloaded: 0, total: Infinity },
-                            merged: { frame: 0, speed: '0x', fps: 0 },
-                        };
-
-                        // Get audio and video streams
-                        const audio = ytdl(ref, { quality: 'highestaudio' })
-                            .on('progress', (_, downloaded, total) => {
-                                tracker.audio = { downloaded, total };
-                            });
-                        const video = ytdl(ref, { quality: 'highestvideo' })
-                            .on('progress', (_, downloaded, total) => {
-                                tracker.video = { downloaded, total };
-                            });
-
-                        // Prepare the progress bar
-                        let progressbarHandle = null;
-                        const progressbarInterval = 1000;
-                        const showProgress = () => {
-                            readline.cursorTo(process.stdout, 0);
-                            const toMB = i => (i / 1024 / 1024).toFixed(2);
-
-                            process.stdout.write(`Audio  | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
-                            process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
-
-                            process.stdout.write(`Video  | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
-                            process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
-
-                            process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
-                            process.stdout.write(`(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${' '.repeat(10)}\n`);
-
-                            process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
-                            readline.moveCursor(process.stdout, 0, -3);
-                        };
-
-                        // Start the ffmpeg child process
-                        const ffmpegProcess = cp.spawn(ffmpeg, [
-                            // Remove ffmpeg's console spamming
-                            '-loglevel', '8', '-hide_banner',
-                            // Redirect/Enable progress messages
-                            '-progress', 'pipe:3',
-                            // Set inputs
-                            '-i', 'pipe:4',
-                            '-i', 'pipe:5',
-                            // Map audio & video from streams
-                            '-map', '0:a',
-                            '-map', '1:v',
-                            // Keep encoding
-                            '-c:v', 'copy',
-                            // Define output file
-                            'out.mkv',
-                        ], {
-                            windowsHide: true,
-                            stdio: [
-                                /* Standard: stdin, stdout, stderr */
-                                'inherit', 'inherit', 'inherit',
-                                /* Custom: pipe:3, pipe:4, pipe:5 */
-                                'pipe', 'pipe', 'pipe',
-                            ],
-                        });
-                        ffmpegProcess.on('close', () => {
-                            console.log('done');
-                            // Cleanup
-                            process.stdout.write('\n\n\n\n');
-                            clearInterval(progressbarHandle);
-                        });
-
-                        // Link streams
-                        // FFmpeg creates the transformer streams and we just have to insert / read data
-                        ffmpegProcess.stdio[3].on('data', chunk => {
-                            // Start the progress bar
-                            if (!progressbarHandle) progressbarHandle = setInterval(showProgress, progressbarInterval);
-                            // Parse the param=value list returned by ffmpeg
-                            const lines = chunk.toString().trim().split('\n');
-                            const arg = {};
-                            for (const l of lines) {
-                                const [key, value] = l.split('=');
-                                arg[key.trim()] = value.trim();
-                            }
-                            tracker.merged = arg;
-                        });
-                        audio.pipe(ffmpegProcess.stdio[4]);
-                        video.pipe(ffmpegProcess.stdio[5]);
-                            await conn.sendMessage(
-                                from,
-                                fs.readFileSync('out.mkv'),
-                                MessageType.video,
-                                { mimetype: Mimetype.mp4, caption: `😪😪`, quoted: mek }
-                            )
-                            console.log("Sent ")
-                            fs.unlinkSync('out.mkv')
-                        break
                     case 'yt':
                         if (!isGroup) return;
                         var url = args[0];
