b10fa0c8105dee894f33f81cc52b851dea8df151
Mahesh Kumar <81576386+jacktheboss220@users.noreply.github.com>
2022-02-13 09:59:47 +0530

    yt best quality

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const ud = require('urban-dictionary')
 const inshorts = require('inshorts-api');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+const cp = require('child_process');
+const readline = require('readline');
+const ffmpeg = require('ffmpeg-static');
 const yahooStockPrices = require('yahoo-stock-prices');
 const port = process.env.PORT || 8000;
 server.get('/', (req, res) => { res.send('V-Bot server running...') })
@@ -678,7 +681,7 @@ async function main() {
 
                     case 'a':
                     case 'alive':
-                        if(!isGroup) return;
+                        if (!isGroup) return;
                         reply("Yes vro");
                         break
                     case 'link':
@@ -1021,9 +1024,96 @@ async function main() {
                         break;
 
 
+                    case 'yth':
+                        if (!isGroup) return;
+                        const ref = args[0];
+                        console.log(`${url}`)
+
+                        // Global constants
+                        const tracker = {
+                            start: Date.now(),
+                            audio: { downloaded: 0, total: Infinity },
+                            video: { downloaded: 0, total: Infinity },
+                        };
+
+                        // Get audio and video stream going
+                        const audio = ytdl(ref, { filter: 'audioonly', quality: 'highestaudio' })
+                            .on('progress', (_, downloaded, total) => {
+                                tracker.audio = { downloaded, total };
+                            });
+                        const video = ytdl(ref, { filter: 'videoonly', quality: 'highestvideo' })
+                            .on('progress', (_, downloaded, total) => {
+                                tracker.video = { downloaded, total };
+                            });
+
+                        // Get the progress bar going
+                        const progressbar = setInterval(() => {
+                            readline.cursorTo(process.stdout, 0);
+                            const toMB = i => (i / 1024 / 1024).toFixed(2);
+
+                            process.stdout.write(`Audio | ${(tracker.audio.downloaded / tracker.audio.total * 100).toFixed(2)}% processed `);
+                            process.stdout.write(`(${toMB(tracker.audio.downloaded)}MB of ${toMB(tracker.audio.total)}MB).${' '.repeat(10)}\n`);
+
+                            process.stdout.write(`Video | ${(tracker.video.downloaded / tracker.video.total * 100).toFixed(2)}% processed `);
+                            process.stdout.write(`(${toMB(tracker.video.downloaded)}MB of ${toMB(tracker.video.total)}MB).${' '.repeat(10)}\n`);
+
+                            process.stdout.write(`running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(2)} Minutes.`);
+                            readline.moveCursor(process.stdout, 0, -2);
+                        }, 1000);
+
+                        // Start the ffmpeg child process
+                        const ffmpegProcess = cp.spawn(ffmpeg, [
+                            // Remove ffmpeg's console spamming
+                            '-loglevel', '0', '-hide_banner',
+                            // 3 second audio offset
+                            '-itsoffset', '3.0', '-i', 'pipe:3',
+                            '-i', 'pipe:4',
+                            // Rescale the video
+                            '-vf', 'scale=320:240',
+                            // Choose some fancy codes
+                            '-c:v', 'libx265', '-x265-params', 'log-level=0',
+                            '-c:a', 'flac',
+                            // Define output container
+                            '-f', 'matroska', 'pipe:5',
+                        ], {
+                            windowsHide: true,
+                            stdio: [
+                                /* Standard: stdin, stdout, stderr */
+                                'inherit', 'inherit', 'inherit',
+                                /* Custom: pipe:3, pipe:4, pipe:5 */
+                                'pipe', 'pipe', 'pipe',
+                            ],
+                        });
+                        ffmpegProcess.on('close', () => {
+                            process.stdout.write('\n\n\n');
+                            clearInterval(progressbar);
+                            console.log('done');
+                        });
+
+                        // Link streams
+                        // FFmpeg creates the transformer streams and we just have to insert / read data
+                        audio.pipe(ffmpegProcess.stdio[3]);
+                        video.pipe(ffmpegProcess.stdio[4]);
+                        ffmpegProcess.stdio[5].pipe(fs.createWriteStream('out.mkv'));
+                        await new Promise((resolve, reject) => {
+                            stream.on('error', reject)
+                            stream.on('finish', resolve)
+                        }).then(async (res) => {
+                            await conn.sendMessage(
+                                from,
+                                fs.readFileSync('out.mkv'),
+                                MessageType.video,
+                                { mimetype: Mimetype.mp4, caption: `😪😪`, quoted: mek }
+                            )
+                            console.log("Sent ")
+                            fs.unlinkSync('out.mkv')
+                        }).catch((err) => {
+                            reply('Video is more then 40mb try -yt to download');
+                        });
+                        break
                     case 'yt':
                         if (!isGroup) return;
-                        var url = args[0]; 
+                        const ref = args[0];
                         console.log(`${url}`)
                         const dm = async (url) => {
                             let info = ytdl.getInfo(url)
@@ -1043,7 +1133,6 @@ async function main() {
                                 )
                                 console.log("Sent ")
                                 fs.unlinkSync(rany)
-
                             }).catch((err) => {
                                 reply('Unable to download,contact dev.');
                             });
