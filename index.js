867689627726fd26d77e8e144c48af1029bb43b5
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2022-03-16 08:58:13 +0530

    idp fixed

--- index.js
+++ index.js
@@ -850,60 +850,31 @@ async function main() {
                         break;
 
                     case 'idp':
+                        if (!isGroup) return;
+                        if (!args[0]) return reply(`_Enter User name after idp_`);
                         let prof = args[0];
-                        axios.get(`https://www.instagram.com/${prof}/?__a=1`, {
-                            headers: {
-                                accept:
-                                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                                'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
-                                'cache-control': 'max-age=0',
-                                'sec-ch-ua':
-                                    '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-                                'sec-ch-ua-mobile': '?1',
-                                'sec-fetch-dest': 'document',
-                                'sec-fetch-mode': 'navigate',
-                                'sec-fetch-site': 'none',
-                                'sec-fetch-user': '?1',
-                                'upgrade-insecure-requests': '1',
-                                cookie:
-                                    'ig_did=305179C0-CE28-4DCD-847A-2F28A98B7DBF; ig_nrcb=1; mid=YQBN3wAEAAGfSSDsZYS9nf2a5MHO; csrftoken=KItbBYAsObQgmJU2CsfqfiRFtk8JXwgm; sessionid=29386738134%3A8NwzjrA3jruVB4%3A23; ds_user_id=29386738134; fbm_124024574287414=base_domain=.instagram.com; shbid="18377\05429386738134\0541674226938:01f7d2db0f9c512fc79336716e1cf02623129a7897f5ccb8d878999be86c0e010bb77920"; shbts="1642690938\05429386738134\0541674226938:01f73e613a6030436ef5f2cea6c7402b82a96c1a61f905b746d3951f49a7f2d2eab6d399"; fbsr_124024574287414=Ps5NinG2AjNMV4W927e_vwMrZVLCltfcbWGS3B5S3to.eyJ1c2VyX2lkIjoiMTAwMDA5NDY1ODIwODQyIiwiY29kZSI6IkFRQlZrOVljMF9DS24tVEpqZ21VWjdPT2dOelFVdkJyLXUzaENSOGR0RzZrbVQxdWszYUMtVDZJeV9QWjBCc1lCcTBmZkxNZmsyUVlMM0hMVGVhQ1pxb1RRQzdsOE9BYlZKdmlvTU5GZ0dncVdxZVQzNV9JM3ZOV0pCR3BsWXVQX0dGMDJMMEt2aTk4WXpxNFhrVWhaVUNRanpPcUthN01aOVdZaVc5SVFzZjRxU3FQTXUzVXlwRWVsMXQ4TjJkV2ZHSnNFYXRsNXBIRXBGMlJSSWljY0F1c3BTZHNPdWFZSThCeV9uRFpjQklUUFk0RzNJY0NiYnFtdXNFZXY5ZUlsMVlZQ0E0bE5ROWxyeGtZdU1IM05scWRFTmtlQjNwWVRjRGlsZDZtekNpNFgzcnZIZUtUMFVFNkJFYVlURFpCTmhaOTd5TmJWT1R1ZENWdk84UlFoYjV2Iiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQU0zaHBjU2lKUm50WWcyTm0xamhlUlFkd3VCeExaQ1V0UjV5endGSkdVQVpDbERGRThwdXdaQXRPMkxtQnMxNjNiVGQzZERhRVl3UGRiWHY1bE5PNEZaQVVoYUpBZDBIcTQyWkN5OVdicXh4blVnZml5MHBETm9rMXlQVzlUNHpaQVVsbHVGcmZ4OFFhRlRnZG9wRTBFMDBMaGg3OVhuWkN1QldteWZ0MlpBY1NYVUpMRjNWNzUwWkQiLCJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTY0MjY5NDAyM30; rur="VLL\05429386738134\0541674231548:01f7816fe2a5156acdb86c5eff76c0ae83ac053646c44ccc592f854fb9d24a18bfcfc3ac"',
-                            },
-                            referrerPolicy: 'strict-origin-when-cross-origin',
-                            body: null,
-                            method: 'GET',
-                            mode: 'cors',
-                        }).then((res) => {
-                            console.log(res.data.graphql.user.profile_pic_url_hd);
-                            const downurl = (res.data.graphql.user.profile_pic_url_hd);
-                            downImage(downurl);
-
-                        }).catch((err) => {
-                            reply("Bad Luck....");
-                        })
-                        const downImage = async (url) => {
-                            const ran = getRandom('.jpg');
-                            const writer = fs.createWriteStream(ran)
-                            const response = await axios({
-                                url,
-                                method: 'GET',
-                                responseType: 'stream'
-                            })
-                            response.data.pipe(writer)
-                            return new Promise((resolve, reject) => {
-                                writer.on('finish', resolve)
-                                writer.on('error', reject)
-                            }).then(async (res) => {
+                        const idp = async (prof) => {
+                            axios({
+                                url: `https://www.instagram.com/${prof}/?__a=1`,
+                                method: 'GET'
+                            }).then((res) => {
+                                reply(`_Searching User..._`);
                                 await conn.sendMessage(
                                     from,
-                                    fs.readFileSync(ran), // send directly from remote url!
+                                    { url: res.data.graphql.user.profile_pic_url_hd },
                                     MessageType.image,
-                                    { mimetype: Mimetype.jpg, caption: `${prof}   ~Blender👽`, quoted: mek }
+                                    {
+                                        mimetype: Mimetype.jpg,
+                                        caption: `${prof}   ~Blender👽`,
+                                        quoted: mek
+                                    }
                                 )
-                                fs.unlinkSync(ran);
-                            }).catch(err => {
-                                reply(`Unexpected Downfall,can Retry after 5 sec..`);
-                            });
+                            }).catch((error) => {
+                                console.log(error);
+                                reply(`_Bad Luck_ :(\nUser name not found!!`);
+                            })
                         }
+                        idp(prof);
                         break;
 
                     case 'fact':
